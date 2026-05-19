076889d05ce623cdaf767903746f1893db60b6b6
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2026-05-19 19:32:12 +0200

    initialize Redux state management for counter feature

--- 01-learn-modern-redux/src/App.tsx
+++ 01-learn-modern-redux/src/App.tsx
@@ -1,7 +1,26 @@
 import "./App.css";
+import { useAppDispatch, useAppSelector } from "./features/hooks";
+import {
+  increment,
+  decrement,
+  reset,
+  amountAdded,
+} from "./features/counter-slice";
 
 function App() {
-  return <h1>Redux toolkit</h1>;
+  const dispatch = useAppDispatch();
+  const count = useAppSelector((state) => state.counter.value);
+
+  return (
+    <div className="App">
+      <h1>Redux toolkit</h1>
+      <p>{count}</p>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
+      <button onClick={() => dispatch(amountAdded(5))}>Add 5</button>
+    </div>
+  );
 }
 
 export default App;
